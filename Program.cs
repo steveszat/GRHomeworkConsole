9aa2925b69bc36334260c2395e5474d3f5c21f57
steveszat <steve.szatkowski@gmail.com>
2022-03-09 10:58:21 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GRHWConsole;
+
+if (args.Length == 0)
+{
+    // Normally this and other messages would be stored elsewhere
+    // to make them easy to modify without having to recompile
+    Console.WriteLine("You must provide a file path parameter");
+}
+else
+{
+    if (File.Exists(args[0]))
+    {
+        Console.WriteLine($"Reading data from {args[0]}");
+
+        try
+        {
+            string[] lines = await File.ReadAllLinesAsync(args[0]);
+
+            var data =
+                (from line in
+                     from line in lines
+                     select line.Split(',')
+                              .ToArray<string>()
+                 select new SomeData()
+                 {
+                     LastName = line[(int)ArrayElement.LastName],
+                     FirstName = line[(int)ArrayElement.FirstName],
+                     Email = line[(int)ArrayElement.Email],
+                     FavoriteColor = line[(int)ArrayElement.FavoriteColor],
+                     DateOfBirth = DateTime.Parse(line[(int)ArrayElement.DoB])
+                 })
+                 .ToList<SomeData>();
+            var check = data;
+            if (args.Length > 1)
+            {
+                Console.WriteLine($"Sort by {args[1]}");
+                string sortBy = args[1];
+                switch(sortBy.ToLower())
+                {
+                    case "color":
+                        data = data.OrderBy(d => d.FavoriteColor)
+                            .ThenBy(d => d.LastName)
+                            .ToList<SomeData>();
+                        break;
+                    case "birthdate":
+                        data = data.OrderBy(d => d.DateOfBirth)
+                        .ToList<SomeData>();
+                        break;
+                    case "name":
+                        data = data.OrderByDescending(d => d.LastName)
+                        .ToList<SomeData>();
+                        break;
+             
+                }
+
+            }
+            foreach (var line in data)
+            {
+                Console.WriteLine(line);
+            }
+
+            Console.WriteLine("Done");
+        }
+        // for sake of brevity, I am only catching generic exception
+        catch (Exception)
+        {
+
+            throw;
+        }
+
+
+    }
+    else
+    {
+        Console.WriteLine("The file specified does not exist");
+    }
+
+}
+
+
+
---
+++ SomeData.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GRHWConsole
+{
+    public class SomeData
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string FavoriteColor { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {Email} {FavoriteColor} {DateOfBirth.ToString("M/d/yyyy")}";
+        }
+    }
+}
